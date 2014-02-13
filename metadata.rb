88706e9920d7b7534334566343290999dcaf7921
Max Korenkov <mkorenkov@gmail.com>
2014-02-13 19:51:04 +0100

    no depends

--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,6 @@ version          '0.1.2.1'
 supports 'redhat'
 supports 'centos'
 
-depends 'java', '~> 1.19.2'
+#depends 'java', '~> 1.19.2'
 #depends 'supervisor', '~> 0.4.10'
 #depends 'logrotate', '~> 1.5.0'

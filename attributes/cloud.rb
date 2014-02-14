a6bf63d392e913f9cbe0a361013e30f28a64cbcb
Max Korenkov <mkorenkov@gmail.com>
2014-02-14 17:01:23 +0100

    creation of node dir changes

--- attributes/cloud.rb
+++
@@ -1,9 +0,0 @@
-default['solr']['zookeeper']['cluster_servers'] = ['zoo1.example.com:2181', 'zoo2.example.com:2181', 'zoo3.example.com:2181']
-default['solr']['cloud']['shards'] = ['shard1', 'shard2']
-default['solr']['cloud']['replicas'] = []
-default['solr']['java']['xmx'] = "3072M"
-default['solr']['java']['xms'] = "512M"
-default['solr']['collection']['name'] = "example"
-default['solr']['bootstrap_conf_dir'] = "./solr/conf"
-default['solr']['log_dir'] = "/var/log/solr"
-
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,8 @@
 #
 
 include_recipe 'java'
+include_recipe "supervisor::default"
+include_recipe "logrotate::default"
 
 src_filename = ::File.basename(node['solr']['url'])
 src_filepath = "#{Chef::Config['file_cache_path']}/#{src_filename}"
@@ -29,5 +31,38 @@ directory node['solr']['data_dir'] do
   owner 'root'
   group 'root'
   action :create
+end
+
+solr_nodes = node['solr']['cloud']['shards'] + node['solr']['cloud']['replicas']
+solr_nodes.each do |node|
+  bash "create #{node} config dir" do
+    user "root"
+    cwd node['solr']['dir']
+    code <<-EOH
+        cp -r example $node
+    EOH
+    environment ({'node' => node})
+  end
+end
+
+template '/etc/supervisor.d/solr.conf' do
+  source 'supervisor.conf.erb'
+  owner  'root'
+  group  'root'
+  mode   '0644'
+  variables({
+    :instances => solr_nodes
+  })
+end
+
+template '/etc/logrotate.d/solr' do
+  source 'logrotate.conf.erb'
+  owner  'root'
+  group  'root'
+  mode   '0644'
+end
+
+execute 'supervisorctl update' do
+  user 'root'
 end
 
